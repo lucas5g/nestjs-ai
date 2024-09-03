952660bff06aec26f186a5ea779f280555c98a06
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-09-03 17:03:06 -0300

    upload file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import { DownloadAppDto } from '@/dto/app.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return 'teste';
+  @Post('/download')
+  async download(@Body() downloadAppDto: DownloadAppDto, @Res() res: Response) {
+    const filePath = await this.appService.download(downloadAppDto);
+
+    res.download(filePath);
+  }
+
+  @Post('/transcribe')
+  @UseInterceptors(FileInterceptor('file'))
+  transcribe(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.transcribe(file);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,28 +1,40 @@
 import Groq from 'groq-sdk';
 import * as youtubeDl from 'youtube-dl-exec';
 
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import { env } from '@/utils/env';
+import { DownloadAppDto } from '@/dto/app.dto';
 @Injectable()
 export class AppService {
-  async transcribe() {
+  async transcribe(file: Express.Multer.File) {
+    if (!file) {
+      return new BadRequestException('Arquivo não entregue');
+    }
     const groq = new Groq({
       apiKey: env.GROQ_API_KEY,
     });
+
+    const filePath = `uploads/${file.originalname}`;
+
+    fs.writeFileSync(filePath, file.buffer);
+
     const transcription = await groq.audio.transcriptions.create({
-      file: fs.createReadStream(`${__dirname}/test.mp4`),
+      file: fs.createReadStream(filePath),
       model: 'whisper-large-v3',
-      response_format: 'verbose_json',
+      // response_format: 'verbose_json',
     });
 
     return transcription;
   }
 
-  download(url: string) {
-    return youtubeDl.exec(url, {
+  async download({ url }: DownloadAppDto) {
+    const pathFile = `uploads/${url.split('=')[1]}.mp4`;
+    await youtubeDl.exec(url, {
       format: 'best',
-      output: `${__dirname}/uploads/${url.split('=')[1]}.mp4`,
+      output: pathFile,
     });
+
+    return pathFile;
   }
 }
